9842acaac731df2c0d399410278456cef185561c
bywhite <bywhite@cisco.com>
2022-12-04 14:58:20 -0600

    Set local org_moid variable

--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@
 
 locals {
   # Intersight Organization Variable
-  org_moid = data.intersight_organization_organization.ofl.moid
+  org_moid = data.intersight_organization_organization.ofl.id
   
   pod_policy_prefix = "dev-ofl-pod1" 
 #  policy_prefix = "dev-ofl-pod1"      >>used in pod1-vmw-1.tf to create IMM Domain
