48dc1c1a24632bc86171684b3a1e3680424b8688
bywhite <bywhite@cisco.com>
2022-12-04 22:03:29 -0600

    Updated Outputs with MacPool MOID

--- outputs.tf
+++ outputs.tf
@@ -7,13 +7,19 @@ output "org_ofl_moid" {
     description = "moid of target Organization in Intersight"
 }
 
-output "ofl_pod1_ip_pool_moid" {
-    value = module.imm_pool_mod.ip_pool_moid
-    description = "This is the moid of the Pod wide IP_Pool for KVM & Chassis in-band IP's"
-}
-
 output "pod_id" {
     value = local.pod_id
     description = "Pod ID is used in all identifiers: MAC, WWNN, WWPN, UUID"
 }
 
+
+output "ofl_pod1_ip_pool_moid" {
+    value = module.imm_pool_mod.ip_pool_moid
+    description = "This is the moid of the Pod wide IP_Pool for KVM & Chassis in-band IP's"
+}
+
+
+output "ofl_pod1_ip_pool_moid" {
+    value = module.imm_pool_mod.mac_pool_moid
+    description = "This is the moid of the Pod wide Mac_Pool for vNic's"
+}
