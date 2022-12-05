252a5842120797030c22d4c2c37ea5dc3c285de3
bywhite <bywhite@cisco.com>
2022-12-04 21:44:03 -0600

    Updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,4 @@
 
-output "ofl_pod1_ip_pool_moid" {
-    value = module.imm_pool_mod.ip_pool_moid
-    description = "This is the moid of the Pod wide IP_Pool for KVM & Chassis in-band IP's"
-}
 
 # print default org moid
 output "org_ofl_moid" {
@@ -11,7 +7,13 @@ output "org_ofl_moid" {
     description = "moid of target Organization in Intersight"
 }
 
+output "ofl_pod1_ip_pool_moid" {
+    value = module.imm_pool_mod.ip_pool_moid
+    description = "This is the moid of the Pod wide IP_Pool for KVM & Chassis in-band IP's"
+}
+
 output "pod_id" {
     value = local.pod_id
     description = "Pod ID is used in all identifiers: MAC, WWNN, WWPN, UUID"
 }
+
