d32ad8a20fbc1822c60a12a9434bbefc62cad4f4
bywhite <bywhite@cisco.com>
2022-12-07 17:27:05 -0600

    updated outputs

--- outputs.tf
+++ outputs.tf
@@ -12,11 +12,6 @@ output "pod_id" {
     description = "Pod ID is used in all identifiers: MAC, WWNN, WWPN, UUID"
 }
 
-
-output "ofl_pod1_ip_pool_moid" {
-    value = module.imm_pool_mod.ip_pool_moid
-    description = "This is the moid of the Pod wide IP_Pool for server IMC Access IP's"
-}
 output "ofl_pod1_ip_pool_chassis_moid" {
     value = module.imm_pool_mod.ip_pool_chassis_moid
     description = "This is the moid of the Pod wide IP_Pool for Chassis in-band IP's"
