ce911050dfa18f3e5dd3a075c23743c8d9475893
bywhite <bywhite@cisco.com>
2022-12-07 17:24:35 -0600

    Cleaned up unused variables in 6536 module call

--- pod-domain-vmw-1.tf
+++ pod-domain-vmw-1.tf
@@ -24,9 +24,6 @@ module "intersight_policy_bundle_vmw_1" {
      #A value of 2 results in 8x 32G FC Port breakouts from ports 35 & 36
      # Unified Ports 33-36 (FC ports on right of the slider starting with 36)
 
-  imc_access_vlan    = 999
-  imc_admin_password = "Cisco123"
-  imc_ip_pool_moid = module.imm_pool_mod.ip_pool_moid
 
   chassis_imc_access_vlan    = 999
   chassis_imc_ip_pool_moid = module.imm_pool_mod.ip_pool_moid
@@ -39,26 +36,13 @@ module "intersight_policy_bundle_vmw_1" {
 
   ntp_timezone = "America/Chicago"
 
-# starting values for wwnn, wwpn-a/b and mac pools (size 255)
-  # wwnn-block   = "20:00:00:CA:FE:00:00:01"
-  # wwpn-a-block = "20:00:00:CA:FE:0A:00:01"
-  # wwpn-b-block = "20:00:00:CA:FE:0B:00:01"
-
-
-#                          01 - 0 is for OFL and 1 is for Pod 1
-#  Need to substitute in Pod ID Variable and not pass starting Blocks
-# sending MOIDs for Identity Pools:  MAC, WWNN, WWPN
-  mac_pool_moid = module.imm_pool_mod.mac_pool_moid
-  wwnn_pool_moid = module.imm_pool_mod.wwnn_pool_moid
-  wwpn_pool_a_moid = module.imm_pool_mod.wwpn_pool_a_moid
-  wwpn_pool_b_moid = module.imm_pool_mod.wwpn_pool_b_moid
-
 
   tags = [
     { "key" : "Environment", "value" : "dev-ofl" },
     { "key" : "Orchestrator", "value" : "Terraform" }
   ]
-
+  
+# The Pools must be created first before this module executes
 depends_on = [
   module.imm_pool_mod
 ]
