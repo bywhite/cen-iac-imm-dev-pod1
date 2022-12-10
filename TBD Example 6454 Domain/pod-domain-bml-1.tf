9c0df350ed5c32dfb3ba78b28ae1fc921062e0ee
bywhite <bywhite@cisco.com>
2022-12-10 01:54:48 -0600

    renamed folders

--- TBD Example 6454 Domain/pod-domain-bml-1.tf
+++ TBD Example 6454 Domain/pod-domain-bml-1.tf
@@ -4,7 +4,7 @@
 
 module "intersight_policy_bundle" {
   #source = "github.com/pl247/tf-intersight-policy-bundle"
-  source = "github.com/bywhite/cen-iac-imm-dev-pod1-mods/imm-domain-fabric-module"
+  source = "github.com/bywhite/cen-iac-imm-dev-pod1-mods/tbd-imm-domain-fabric-6454-mod"
   # external sources
   organization    = data.intersight_organization_organization.ofl.id
 
