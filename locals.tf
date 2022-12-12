1088543709103140b0912004e837fbcd29730d91
bywhite <bywhite@cisco.com>
2022-12-12 14:40:14 -0600

    updated comment

--- locals.tf
+++ locals.tf
@@ -11,8 +11,8 @@ locals {
 #  policy_prefix = "dev-ofl-pod1"      >>used in pod1-vmw-1.tf to create IMM Domain
   
   pod_id = "01"
-#           0 is for OFL    RCO is 1    BUF is 3  other locations TBD
-#           1 is for Pod 1      Pod 2 is 2   Pod 3 is 3  
+#           0 is for OFL    RCO is 1     BUF is 3  other locations TBD
+#           1 is for Pod 1  Pod 2 is 2   Pod 3 is 3    etc. 
 #  Example RCO Pod 2 ID would be:  "12"
 #  All Identity Pools for a Pod will contain the POD ID (MAc, WWNN, WWPN, UUID)
 
