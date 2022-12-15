fb5c72234df83abc7e7f1b1e5a86da59ed765a64
bywhite <bywhite@cisco.com>
2022-12-14 19:44:56 -0600

    cleaned up formatting

--- pod-srv-template-vmw1.tf
+++ pod-srv-template-vmw1.tf
@@ -51,7 +51,7 @@ module "imm_pod_server_vmw_1" {
 #   ]
 
 vnic_vlan_sets = {
-    "eth0"  = {
+    "eth0"  = {           # Needs to match vnic_name
       vnic_name  = "eth0"
       native_vlan = 44
       vlan_range  = "44,50,1000-1011"
