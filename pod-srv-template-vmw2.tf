67d54bf785d199c8bba5e79ef837784edfa51b15
bywhite <bywhite@cisco.com>
2022-12-18 09:50:28 -0600

    recreated vmw2 srv template

--- pod-srv-template-vmw2.tf
+++ pod-srv-template-vmw2.tf
@@ -9,8 +9,8 @@
 # # -----------------------------------------------------------------------------
 
 
-module "server_template_vmw1" {                      # <<-- Change to duplicate
-  source = "github.com/bywhite/cen-iac-imm-dev-pod1-mods/imm-pod-servers-vmw1-mod"
+module "server_template_vmw2" {                      # <<-- Change to duplicate
+  source = "github.com/bywhite/cen-iac-imm-dev-pod1-mods/imm-pod-servers-vmw2-mod"
             # remote module name above should not be changed when duplicating
 
 # =============================================================================
@@ -23,14 +23,14 @@ module "server_template_vmw1" {                      # <<-- Change to duplicate
 # Servers to create from Template
 # -----------------------------------------------------------------------------
 
-  server_count = 0
+  server_count = 2
 
 # =============================================================================
 # Naming and tagging
 # -----------------------------------------------------------------------------
 
   # prefix for all created policies
-  server_policy_prefix = "ofl-dev-pod1-vmw1"         # <<-- Change to duplicate
+  server_policy_prefix = "ofl-dev-pod1-vmw2"         # <<-- Change to duplicate
   description   = "built by Terraform cen-iac-imm-dev-pod1 derived"
 
   #Every object created in the domain will have these tags
@@ -38,7 +38,7 @@ module "server_template_vmw1" {                      # <<-- Change to duplicate
     { "key" : "environment", "value" : "dev" },
     { "key" : "orchestrator", "value" : "Terraform" },
     { "key" : "pod", "value" : "ofl-dev-pod1" },
-    { "key" : "ServerGroup", "value" : "ofl-dev-pod1-vmw1-srvgroup" } # <-- Change
+    { "key" : "ServerGroup", "value" : "ofl-dev-pod1-vmw2-srvgroup" } # <-- Change
   ]
 
 # =============================================================================
@@ -54,21 +54,55 @@ module "server_template_vmw1" {                      # <<-- Change to duplicate
   server_uuid_pool_name    = module.imm_pool_mod.uuid_pool_name
 
 # =============================================================================
-# Server Networking Configurations
+# Server vNic Configurations & FC SAN HBA's
 # -----------------------------------------------------------------------------
 
-vnic_vlan_sets = {
+  vnic_vlan_sets = {
     "eth0"  = {           # Needs to match vnic_name
       vnic_name  = "eth0"
       native_vlan = 45
-      vlan_range  = "45,50,1000-1011"
+      vlan_range  = "45"
       switch_id   = "A"
+      pci_order   = 0
     }
     "eth1"  = {
       vnic_name   = "eth1"
       native_vlan = 45
+      vlan_range  = "45"
+      switch_id   = "B"
+      pci_order   = 1
+    }
+   "eth2"  = {           # Needs to match vnic_name
+      vnic_name  = "eth2"
+      native_vlan = 45
+      vlan_range  = "45,50,1000-1011"
+      switch_id   = "A"
+      pci_order   = 2
+    }
+    "eth3"  = {
+      vnic_name   = "eth3"
+      native_vlan = 45
       vlan_range  = "45,50,1000-1011"
       switch_id   = "B"
+      pci_order   = 3
+    }
+  }
+
+
+  vhba_vsan_sets = {
+    "fc0" = {
+      vhba_name      = "fc0"
+      vsan_id        = 100
+      switch_id      = "A"
+      wwpn_pool_moid = module.imm_pool_mod.wwpn_pool_a_moid
+      pci_order      = 4
+    }
+    "fc1"  = {
+      vhba_name      = "fc1"
+      vsan_id        = 200
+      switch_id      = "B"
+      wwpn_pool_moid = module.imm_pool_mod.wwpn_pool_b_moid
+      pci_order      = 5
     }
   }
 
@@ -92,6 +126,6 @@ vnic_vlan_sets = {
 # The Pools for the Pod must be created before this domain fabric module executes
   depends_on = [
     module.imm_pool_mod
-]
+  ]
 
 }
