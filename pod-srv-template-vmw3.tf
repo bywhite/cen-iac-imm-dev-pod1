8eb40dbdbcf38ef3db33ff45043736314297b007
bywhite <bywhite@cisco.com>
2022-12-18 16:05:38 -0600

    Reduced vmw3 to 2 servers

--- pod-srv-template-vmw3.tf
+++ pod-srv-template-vmw3.tf
@@ -23,7 +23,7 @@ module "server_profiles_vmw3" {                      # <<-- Change to duplicate
 # Servers to create from Template
 # -----------------------------------------------------------------------------
 
-  server_count = 3
+  server_count = 2
 
 # =============================================================================
 # Naming and tagging
