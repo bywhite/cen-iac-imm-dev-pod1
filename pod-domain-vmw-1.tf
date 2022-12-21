b3a091785aa2eddaf1a9030c1eab2b287a64e9ce
bywhite <bywhite@cisco.com>
2022-12-20 18:43:26 -0600

    setting 1-30 for server ports on vmw

--- pod-domain-vmw-1.tf
+++ pod-domain-vmw-1.tf
@@ -45,7 +45,7 @@ module "intersight_policy_bundle_vmw_1" {              # <-- change when copying
   eth_breakout_start = 29
 
   # FI physical port numbers to be attached to chassis 
-  server_ports_6536 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
+  server_ports_6536 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
   
   eth_aggr_server_ports = null
   # eth_aggr_server_ports = {
