90995de4cad717ebc08430b9011e66a86b336f84
bywhite <bywhite@cisco.com>
2023-01-10 15:46:36 -0600

    Use 3 versions of Module per v1.1.1

--- pod-domain-vmw-2.tf
+++ pod-domain-vmw-2.tf
@@ -7,7 +7,7 @@
 
 
 module "intersight_policy_bundle_vmw_2" {              # <-- change when copying
-  source = "github.com/bywhite/cen-iac-imm-dev-pod1-mods/imm-domain-fabric-6536"
+  source = "github.com/bywhite/cen-iac-imm-dev-pod1-mods//imm-domain-fabric-6536?ref=v1.1.1c"
 
 # =============================================================================
 # Org external references
