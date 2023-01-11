d3d9f852069b43b5ab4dd58c0a89182c2ffe62ea
bywhite <bywhite@cisco.com>
2023-01-11 16:43:10 -0600

    added org var

--- X1-pod-pools-config.tf
+++ X1-pod-pools-config.tf
@@ -2,6 +2,9 @@
 module "imm_pool_mod_x1" {
   source = "github.com/bywhite/cen-iac-imm-dev-pod1-mods/x1-pool-mod"
 
+    # external sources
+  organization    = local.org_moid
+
   pod_id = "11"
   # used to create moids for Pools: MAC, WWNN, WWPN
 
--- X1-pod-pools-x1.tf
+++ X1-pod-pools-x1.tf
@@ -1,5 +1,6 @@
 # resource "intersight_macpool_pool" "cisco_af_1" {
 #   name = "cisco_af_1"
+#   organization    = local.org_moid
 
 #   mac_blocks {
 #     from = "00:25:B5:AF:10:00"
