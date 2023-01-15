b9f72873778b612e7e2c89832dc0175b14525be2
bywhite <bywhite@cisco.com>
2023-01-14 20:21:14 -0600

    updating module call version

--- pod-domain-6536-lnx-1.tf
+++ pod-domain-6536-lnx-1.tf
@@ -8,7 +8,7 @@
 
 module "intersight_policy_bundle_lnx_1" {             # <-- change when copying
   #source = "github.com/pl247/tf-intersight-policy-bundle"
-  source = "github.com/bywhite/cen-iac-imm-dev-pod1-mods//imm-domain-fabric-6536?ref=v1.1.1b"
+  source = "github.com/bywhite/cen-iac-imm-dev-pod1-mods//imm-domain-fabric-6536"  #?ref=v1.1.1b"
 
 # =============================================================================
 # Org external references
