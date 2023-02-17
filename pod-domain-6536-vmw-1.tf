2a57134a176266967dfb65ab6aff187f9c15300b
bywhite <bywhite@cisco.com>
2023-02-17 09:53:00 -0600

    Reset for Demo

--- pod-domain-6536-vmw-1.tf
+++ pod-domain-6536-vmw-1.tf
@@ -21,7 +21,7 @@ module "intersight_policy_bundle_vmw_1" {              # <-- change when copying
 # -----------------------------------------------------------------------------
 
   # every policy created will have this prefix in its name
-  policy_prefix = "${local.pod_policy_prefix}-vmw-1"                # <-- change when copying
+  policy_prefix = "${local.pod_policy_prefix}-vmw-1"                  # <-- change when copying
   description   = "built by Terraform ${local.pod_policy_prefix}"
 
   #Every object created in the domain will have these tags
@@ -31,6 +31,16 @@ module "intersight_policy_bundle_vmw_1" {              # <-- change when copying
     { "key" : "pod", "value" : "${local.pod_policy_prefix}" },
     { "key" : "domain", "value" : "${local.pod_policy_prefix}-vmw1" } # <-- change when copying
   ]
+
+# =============================================================================
+# Chassis
+# -----------------------------------------------------------------------------
+
+  chassis_9508_count = 5
+  chassis_imc_access_vlan    = 999
+  chassis_imc_ip_pool_moid = module.imm_pool_mod.ip_pool_chassis_moid 
+  # Chassis requires In-Band IP's Only  (ie must be a VLAN trunked to FI's)
+  # Need chassis_imc_access_password from TFCB Workspace Variable
 
 
 # =============================================================================
@@ -146,15 +156,6 @@ module "intersight_policy_bundle_vmw_1" {              # <-- change when copying
     }
   }
 
-# =============================================================================
-# Chassis
-# -----------------------------------------------------------------------------
-
-  chassis_9508_count = 10
-  chassis_imc_access_vlan    = 999
-  chassis_imc_ip_pool_moid = module.imm_pool_mod.ip_pool_chassis_moid 
-  # Chassis requires In-Band IP's Only  (ie must be a VLAN trunked to FI's)
-  # Need chassis_imc_access_password from TFCB Workspace Variable
 
 # =============================================================================
 # NTP, DNS and SNMP Settings
