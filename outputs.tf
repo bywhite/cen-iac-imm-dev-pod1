50921441831a989d01114c679f56ace733743c15
bywhite <bywhite@cisco.com>
2023-03-16 10:59:55 -0500

    cleaned up outputs

--- outputs.tf
+++ outputs.tf
@@ -15,10 +15,10 @@ output "pod_id" {
     description = "Pod ID is used in all identifiers: MAC, WWNN, WWPN, UUID"
 }
 
-output "domain_vmw_1_name" {
-    value       = module.intersight_policy_bundle_vmw_1.fi6536_cluster_domain_name
-    description = "IMM Domain Cluster VMW-1 TF object name"
-}
+# output "domain_vmw_1_name" {
+#     value       = module.intersight_policy_bundle_vmw_1.fi6536_cluster_domain_name
+#     description = "IMM Domain Cluster VMW-1 TF object name"
+# }
 
 # output "vmw_1_chassis_count" {
 #     value       = module.intersight_policy_bundle_vmw_1.chassis_count
@@ -31,15 +31,15 @@ output "domain_vmw_1_name" {
 # }
 
 
-output "domain_vmw_1_profile_name" {
-    value       = module.intersight_policy_bundle_vmw_1.fi6536_cluster_profile_name
-    description = "IMM Domain Cluster VMW-1 TF object name"
-}
+# output "domain_vmw_1_profile_name" {
+#     value       = module.intersight_policy_bundle_vmw_1.fi6536_cluster_profile_name
+#     description = "IMM Domain Cluster VMW-1 TF object name"
+# }
 
-output "domain_vmw_1_moid" {
-    value       = module.intersight_policy_bundle_vmw_1.fi6536_cluster_profile_moid
-    description = "IMM Domain Cluster VMW-1 moid"
-}
+# output "domain_vmw_1_moid" {
+#     value       = module.intersight_policy_bundle_vmw_1.fi6536_cluster_profile_moid
+#     description = "IMM Domain Cluster VMW-1 moid"
+# }
 
 # output "vmw_1_chassis_9508_profile_moids" {
 #     value       = module.intersight_policy_bundle_vmw_1.chassis_9508_profile_moids
